9c588b02d16d7881607e7aac47bcb2edef30e77d
Jackie Kinsler <jackiekinsler@gmail.com>
2020-09-23 21:43:43 +0000

    reverse int problem edge case handling.

---
+++ reverse_int.py
@@ -0,0 +1,31 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        
+        if x == 0:
+            return 0
+        
+        if x < 0: 
+            negative = True 
+            x *= -1
+        else: 
+            negative = False
+            
+        int_str = str(x)
+
+        char_lst = []
+
+        for char in int_str: 
+            char_lst.append(char)
+        
+        reversed_str = ''
+        char_lst.reverse()
+        reversed_str = reversed_str.join(char_lst).strip('0')
+        
+        
+        if negative == True: 
+            return -1*int(reversed_str)
+        else: 
+            return int(reversed_str)
+        
+        
+        
