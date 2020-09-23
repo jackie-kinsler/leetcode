a64aa83c6a311ab02c59259c282bd29ab72119fc
Jackie Kinsler <jackiekinsler@gmail.com>
2020-09-23 21:19:13 +0000

    removed list copy and did everything with slicing instead.

--- twosums.py
+++ twosums.py
@@ -23,11 +23,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        test_lst = nums.copy()
-        
-        for num in nums: 
-            test_num = test_lst.pop()
-            test_set = set(test_lst)
+        len_nums = len(nums)
+        for index in range(1, len_nums + 1): 
+            test_num = nums[len_nums - index]
+            test_set = set(nums[:(len_nums-index)])
             remainder = target - test_num
             if remainder in test_set: 
                 break
