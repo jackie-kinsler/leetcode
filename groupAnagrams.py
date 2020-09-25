9e72d086380d0dc7ce0f3d7312085391945126a5
Jackie Kinsler <jackiekinsler@gmail.com>
2020-09-25 23:42:39 +0000

    got all anagrams to appear in intermediate list. but anagram list has duplicates.

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -12,9 +12,26 @@ def groupAnagrams(strs):
         for char in word: 
             letter_dict[char] = letter_dict.get(char, 0) + 1
         word_dictionary[word] = letter_dict
-
-        print(letter_dict)
+        
     print(word_dictionary)
+    # {
+    # 'eat': {'e': 1, 'a': 1, 't': 1}, 
+    # 'tea': {'t': 1, 'e': 1, 'a': 1}, 
+    # 'tan': {'t': 1, 'a': 1, 'n': 1}, 
+    # 'ate': {'a': 1, 't': 1, 'e': 1}, 
+    # 'nat': {'n': 1, 'a': 1, 't': 1}, 
+    # 'bat': {'b': 1, 'a': 1, 't': 1}
+    # }
+    for key in word_dictionary:
+        intermediate_list = []
+        for word, letters in word_dictionary.items(): 
+            if word_dictionary[key] == letters:
+                intermediate_list.append(word)
+        anagram_list.append(intermediate_list)
+    
+    print(anagram_list)
+
+
 
 
 
