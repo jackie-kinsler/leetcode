e49f7bca19443c9a64239c12cb70b23d548ea805
Jackie Kinsler <jackiekinsler@gmail.com>
2020-09-25 23:06:20 +0000

    Added tests.

--- positionsOfLargeGroups.py
+++ positionsOfLargeGroups.py
@@ -29,36 +29,65 @@
             
 #         return sorted(index_lst)
 
+import unittest
 
-class Solution:
-    def largeGroupPositions(self, s: str) -> List[List[int]]:
-          
-        index_lst = []
-        char_lst = []
-        for char in s: 
-            char_lst.append(char)
 
-        def check_group_length(input_lst): 
-            group = 1
-            while len(input_lst) > 1: 
-                current_char = input_lst.pop()
-                next_char = input_lst[-1]
-                if current_char == next_char: 
-                    group += 1
-                else: 
-                    break
-                if group >= 3 and len(char_lst) == 1:
-                    char_lst.pop()
-                    break
-            return group
+def largeGroupPositions(s):
 
-        while len(char_lst) > 1:
-            group = check_group_length(char_lst)
-            if group >= 3:
-                index_lst.append([len(char_lst), len(char_lst) + group - 1])
+    """Returns list of index positions of large groups.
+        >>> largeGroupPositions("abbxxxxzzy")
+        [[3, 6]]
+        >>> largeGroupPositions("abc")
+        []
+        >>> largeGroupPositions("abcdddeeeeaabbbcd")
+        [[3, 5], [6, 9], [12, 14]]
+        >>> largeGroupPositions("aaa")
+        [[0, 2]]
+    """
         
-        return sorted(index_lst)
+    index_lst = []
+    char_lst = []
+    for char in s: 
+        char_lst.append(char)
 
+    def check_group_length(input_lst): 
+        group = 1
+        while len(input_lst) > 1: 
+            current_char = input_lst.pop()
+            next_char = input_lst[-1]
+            if current_char == next_char: 
+                group += 1
+            else: 
+                break
+            if group >= 3 and len(char_lst) == 1:
+                char_lst.pop()
+                break
+        return group
+
+    while len(char_lst) > 1:
+        group = check_group_length(char_lst)
+        if group >= 3:
+            index_lst.append([len(char_lst), len(char_lst) + group - 1])
+    
+    return sorted(index_lst)
+
+class MyUnittest(unittest.TestCase):
+
+    def test_largeGroupPositions(self): 
+        assert largeGroupPositions("abbxxxxzzy") == [[3, 6]]
+
+    def test_largeGruopPositions_2(self):
+        self.assertEqual(largeGroupPositions("aaa"), [[0, 2]])
+
+if __name__ == '__main__':
+    unittest.main()
+
+# if __name__ == '__main__':
+#     import doctest
+
+#     result = doctest.testmod()
+#     if result.failed == 0:
+#         print('ALL TESTS PASSED')
 
 
             
