6dab13cfab83f814a439ccffccb0e513fdf46237
Jackie Kinsler <jackiekinsler@gmail.com>
2020-10-08 22:26:15 +0000

    Put dictionary values into a list and returned the grouped anagrams.

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -9,8 +9,10 @@ def groupAnagrams(strs):
             words_list = anagram_dictionary.get(sorted_word)
             words_list.append(word)
             anagram_dictionary[sorted_word] = words_list
+    for item in anagram_dictionary.values():
+        anagram_list.append(item)
+    return anagram_list
+print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+print(groupAnagrams([""]))
+print(groupAnagrams(["a"]))
 
-    
-    print(anagram_dictionary)
-
-groupAnagrams(["eat","tea","tan","ate","nat","bat"])
