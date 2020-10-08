58c243494ffc262b7b7d84173303f2692fe88558
Jackie Kinsler <jackiekinsler@gmail.com>
2020-10-08 22:36:38 +0000

    check if letter dictionaries are same, return true or false.

--- validAnagram.py
+++ validAnagram.py
@@ -1,14 +1,14 @@
 def isAnagram(s, t): 
     s_letter_count = {}
     t_letter_count = {}
-
     for char in s: 
         s_letter_count[char] = s_letter_count.get(char, 0) + 1
-
     for char in t: 
         t_letter_count[char] = t_letter_count.get(char, 0) + 1
-    print(s_letter_count)
-    print(t_letter_count)
+    if s_letter_count == t_letter_count:
+        return True
+    return False 
 
-isAnagram('anagram', 'nagaram')
+print(isAnagram('anagram', 'nagaram'))
+print(isAnagram('rat', 'car'))
     
